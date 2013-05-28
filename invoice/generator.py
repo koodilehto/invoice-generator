25a67d87da8de3ba6af5a03294e7425d53e8fb8d
Tuomo Sipola <tuomo.sipola@iki.fi>
2013-05-28 20:40:48 +0300

    Commented the functions

--- invoice/generator.py
+++ invoice/generator.py
@@ -5,6 +5,7 @@ import json
 
 
 def run():
+    """Format the template using the data from the argument files."""
     args = parse_args()
     tpl = read(args.template)
     config = json.loads(read(args.config))
@@ -16,6 +17,7 @@ def run():
 
 
 def parse_args():
+    """Read the command line arguments and return an object containing them."""
     parser = argparse.ArgumentParser(description='Generates invoices')
     parser.add_argument('-c', '--config', type=str,
         help='Configuration file (json)', required=True)
@@ -30,16 +32,19 @@ def parse_args():
 
 
 def read(path):
+    """Read files with UTF safely."""
     with codecs.open(path, encoding='utf-8') as f:
         return f.read()
 
 
 def write(path, data):
+    """Write files with UTF safely."""
     with codecs.open(path, 'w', encoding='utf-8') as f:
         f.write(data)
 
 
 def fieldify(config):
+    """Replace template fields with actual data."""
     class Proxy(dict):
         def __init__(self, a):
             for k, v in a.items():
