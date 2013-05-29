65248e4530c158143a92c7be91185cbe136c639e
Tuomo Sipola <tuomo.sipola@iki.fi>
2013-05-29 22:34:13 +0300

    Todo about file naming scheme

--- demo.sh
+++ demo.sh
@@ -4,6 +4,8 @@
 ./invoice/generator.py -c sender/koodilehto.json -r companies/demo.json -t tex/template.tex -o demo.tex
 
 # Compile the TeX file. Now you should have demo.pdf invoice.
+# TODO: These should be done in the code to generate 
+# a naming scheme for the output file, e.g., sender_recipient_number.pdf
 pdflatex demo.tex
 rm *.aux *.log *.out
 rm demo.tex
