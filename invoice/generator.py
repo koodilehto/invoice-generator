ec8f78c35c0c7a4da3fcdb91f0a6b7eced15c2d1
Juho Vepsalainen <bebraw@gmail.com>
2013-05-30 10:54:37 +0300

    Remove fieldify
    Not going to need that given we're not using string.format anymore.

--- invoice/generator.py
+++ invoice/generator.py
@@ -13,7 +13,7 @@ def run():
     config['recipient'] = json.loads(read(args.recipient))
 
     out = args.output
-    write(out, render(tpl, fieldify(config)))
+    write(out, render(tpl, config))
     print 'Wrote ' + out + '!'
 
 
@@ -62,19 +62,6 @@ def write(path, data):
     """Write files with UTF safely."""
     with codecs.open(path, 'w', encoding='utf-8') as f:
         f.write(data)
-
-
-def fieldify(config):
-    """Fill template field data structure with actual data."""
-    class Proxy(dict):
-        def __init__(self, a):
-            for k, v in a.items():
-                self[k] = Proxy(v) if isinstance(v, dict) else v
-
-        def __getattr__(self, a):
-            return self.get(a, '')
-
-    return Proxy(config)
 
 
 if __name__ == '__main__':
